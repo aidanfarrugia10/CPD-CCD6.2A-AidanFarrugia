31b57463bee0da8a42b7ece5c21f759ec4ef718f
aid-farr <aidanfarrugia2004@gmail.com>
2025-03-25 23:34:03 +0100

    Manual Connection but not populating database

--- pilot_journal/lib/main.dart
+++ pilot_journal/lib/main.dart
@@ -4,7 +4,18 @@ import 'pages/home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyCsLqDMlKMnbUmH1ro2IirWfkxIfLIjG4M",
+      authDomain: "pilotjournal-bacde.firebaseapp.com",
+      projectId: "pilotjournal-bacde",
+      storageBucket: "pilotjournal-bacde.firebasestorage.app",
+      messagingSenderId: "427998652083",
+      appId: "1:427998652083:web:336fa8fb3b265bb8d132c6",
+      databaseURL:
+          "https://pilotjournal-bacde-default-rtdb.europe-west1.firebasedatabase.app",
+    ),
+  );
   runApp(const PilotDestinationTrackerApp());
 }
 
@@ -14,18 +25,18 @@ class PilotDestinationTrackerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Pilot Destination Tracker',
+      title: 'Pilot Journal',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         useMaterial3: true,
       ),
-      debugShowCheckedModeBanner: false,
       home: const HomePage(),
     );
   }
 }
 
-//Destination Object
+// Destination object
 class Destination {
   final String title;
   final String description;
